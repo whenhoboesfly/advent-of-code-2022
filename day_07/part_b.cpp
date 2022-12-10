5df28fb04797edcaad1d969b8f561586acae4b39
whenhoboesfly <119640432+whenhoboesfly@users.noreply.github.com>
2022-12-10 15:35:34 -0600

    Create part_b.cpp

---
+++ day_07/part_b.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <sstream>
+#include <stack>
+using namespace std;
+
+#define TOTAL_DISK_SPACE 70000000
+#define REQUIRED_SPACE 30000000
+
+int toInt(std::string str);
+
+int main(){
+    fstream myfile;
+    string myline;
+    stack<int> cd;
+    int cd_sum = 0;
+    int total_sum = 0;
+    int calculation = 0;
+    string cmd_cd = "$ cd ";
+    string cmd_up = "$ cd ..";
+    string cmd_ls = "$ ls";
+    string lbl_dr = "dir";
+
+    myfile.open("input.txt");
+    if(myfile.is_open()){
+        while(getline(myfile, myline)){
+            //ls
+            if(myline.find(cmd_ls) != std::string::npos){
+            //dir
+            }else if(myline.find(lbl_dr) != std::string::npos){ 
+            //up
+            }else if(myline.find(cmd_up) != std::string::npos){  
+                cd_sum += cd.top();
+                calculation = REQUIRED_SPACE-(TOTAL_DISK_SPACE-cd_sum);
+                if((calculation < total_sum) && (calculation <= -REQUIRED_SPACE)){
+                    total_sum = cd_sum;
+                }
+                cd.pop();
+            }else if(myline.find(cmd_cd) != std::string::npos){ 
+                cd.push(cd_sum);
+                cd_sum = 0;
+            //size
+            }else{             
+                cd_sum += toInt(myline.substr(0, myline.find(" "))); 
+            }
+        }
+    }
+    cout << "Total size of smallest directory to be cleared: " << total_sum  << endl;
+    myfile.close();
+    return 0;
+}
+
+int toInt(std::string str){
+    int num;
+    std::stringstream ss(str);
+    ss >> num;
+    return num;
+}
